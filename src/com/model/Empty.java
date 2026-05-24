2931d1f4502740b7eb4df1809d4e0ec18eff5ea4
Ezgi Erken <ezgii.erkenn@gmail.com>
2026-05-24 19:44:27 +0300

    Added road and empty cell

---
+++ src/com/model/Empty.java
@@ -0,0 +1,8 @@
+package com.model;
+//Empty cell in the map.
+
+public class Empty extends Cell{
+    public Empty(int row, int col){
+        super(row,col,'E');
+    }
+}
---
+++ src/com/model/Road.java
@@ -0,0 +1,8 @@
+package com.model;
+//Road cell used for utility distribution.
+
+public class Road extends Cell{
+    public Road(int row, int col){
+        super(row,col,'R');
+    }
+}
