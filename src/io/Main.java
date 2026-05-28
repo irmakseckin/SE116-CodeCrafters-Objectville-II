f0aa11488f05fbe9e0dcc7d41346acc24d97277e
Teoman Uyuğ <teomanuyug.gmail.com>
2026-05-28 11:37:36 +0300

    implement map parsing and initialize char grid

--- src/io/Main.java
+++ src/io/Main.java
@@ -1,9 +1,15 @@
+package io;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[]args){
+
+
+        ArrayList<String> lines = new ArrayList<>();
 
         System.out.println("Starting ObjectVille... ");
 
@@ -13,9 +19,24 @@ public class Main {
 
             while((line = br.readLine()) != null){
 
-                System.out.println(line);
+                lines.add(line);
+
             }
 
+            int rows = lines.size();
+            int cols = lines.get(0).length();
+
+
+            char[][] grid = new char[rows][cols];
+
+            for (int i = 0; i < rows; i++) {
+                String currentLine = lines.get(i);
+
+                for (int j = 0; j < cols; j++) {
+                    grid[i][j] = currentLine.charAt(j);
+
+                }
+            }
 
 
         }catch(IOException e){
