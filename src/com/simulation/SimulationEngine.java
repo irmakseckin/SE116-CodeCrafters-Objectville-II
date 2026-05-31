d191fd5304b9ffb56b297d5115e0aaa41e81bcd0
Irmak Seckin <irmaksecckinn@gmail.com>
2026-05-31 22:39:03 +0300

    Added resource distribution and production collection

--- src/com/simulation/SimulationEngine.java
+++ src/com/simulation/SimulationEngine.java
@@ -2,13 +2,23 @@ package com.simulation;
 import com.model.Cell;
 import com.model.Zone;
 import com.model.ServiceBuilding;
+import com.model.Housing;
+import com.model.Industrial;
+import com.model.Commercial;
 
 
 public class SimulationEngine {
     private Cell[][] grid;
+    private int totalPopulation;
+    private int totalGoods;
+    private int totalLifestyle;
+
 
     public SimulationEngine(Cell[][] grid) {
         this.grid = grid;
+        this.totalPopulation = 0;
+        this.totalGoods = 0;
+        this.totalLifestyle = 0;
     }
 
     public void run(int tickCount){
@@ -16,7 +26,9 @@ public class SimulationEngine {
             System.out.println("Tick " + tick);
             resetZones();
             distributeServices();
+            distributeResources();
             updateZones();
+            collectProduction();
         }
     }
 
@@ -97,6 +109,62 @@ public class SimulationEngine {
     }
 
     public void distributeResources(){
+        int workZoneCount=0;
+        int commercialCount=0;
+        int housingCount=0;
+
+        for(int row=0; row<grid.length; row++){
+            for(int col=0; col<grid[row].length; col++){
+                char symbol=grid[row][col].getSymbol();
+
+                if(symbol == 'I' || symbol == 'C'){
+                    workZoneCount++;
+                }
+                if(symbol == 'C'){
+                    commercialCount++;
+                }
+                if(symbol == 'H'){
+                    housingCount++;
+                }
+            }
+        }
+        int populationAmount=0;
+        int goodsAmount=0;
+        int lifestyleAmount=0;
+
+        if(workZoneCount>0){
+            populationAmount=totalPopulation/workZoneCount;
+        }
+
+        if(commercialCount>0){
+            goodsAmount=totalGoods/commercialCount;
+        }
+
+        if(housingCount>0){
+            lifestyleAmount=totalLifestyle/housingCount;
+        }
+        for(int row=0;row<grid.length;row++){
+            for(int col=0; col<grid[row].length;col++){
+                char symbol= grid[row][col].getSymbol();
+
+                if (symbol == 'I'){
+                    Industrial industrial=(Industrial) grid[row][col];
+                    industrial.setAvailablePopulation(populationAmount);
+                    }
+                if(symbol == 'C'){
+                    Commercial commercial= (Commercial) grid[row][col];
+                    commercial.setAvailableGoods(goodsAmount);
+                    commercial.setAvailablePopulation(populationAmount);
+                }
+                if(symbol == 'H'){
+                    Housing housing=(Housing) grid[row][col];
+                    housing.setLifestyleReceived(lifestyleAmount);
+                }
+
+            }
+        }
+
+
 
     }
 
@@ -115,6 +183,30 @@ public class SimulationEngine {
     }
 
     public void collectProduction(){
+        totalPopulation=0;
+        totalGoods=0;
+        totalLifestyle=0;
+
+        for(int row=0; row< grid.length; row++){
+            for(int col=0; col<grid[row].length; col++){
+                char symbol = grid[row][col].getSymbol();
+
+                if (symbol == 'H'){
+                    Housing housing=(Housing) grid[row][col];
+                    totalPopulation+=housing.calculateOutput();
+                }
+
+                if(symbol == 'I'){
+                    Industrial industrial=(Industrial) grid[row][col];
+                    totalGoods+=industrial.calculateOutput();
+                }
+
+                if(symbol == 'C'){
+                    Commercial commercial=(Commercial) grid[row][col];
+                    totalLifestyle+=commercial.calculateOutput();
+                }
+            }
+        }
 
     }
 
